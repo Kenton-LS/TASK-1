fe382b2e1e94f6a76a27a133d3234dc0f2fc0f2d
Kenton <kentonlesliesmith@gmail.com>
2019-10-03 21:15:56 +0200

    Updated GameEngine
    Boundary Checking has been implemented. Likewise with an efficient gameloop,  and getUnitInfo() t0 implement data to the rich box string.

--- TASK 1/Task 1/Task 1/GameEngine.cs
+++ TASK 1/Task 1/Task 1/GameEngine.cs
@@ -8,12 +8,116 @@ namespace Task_1
 {
     public class GameEngine
     {
-        public int finishedRounds = 0;
-        bool finishedActions = false;
+        Map map; //filed for map
+        bool gameOver = false; //set to true if game ends
+        string winning = ""; //winning faction string
+        int round = 0;
 
-        Map map = new Map();
-        MeleeUnit meleeUnit = new MeleeUnit(0, 0, 7, 7, 2, 4, 1, "Melee", "M");
-        RangedUnit rangedUnit = new RangedUnit(0, 0, 5, 5, 4, 2, 3, "Ranged", "R");
+        public GameEngine()
+        {
+            map = new Map(10); //number of units
+        }
 
+        public bool GameOver
+        {
+            get { return gameOver; }
+        }
+
+        public string Winning
+        {
+            get { return winning; }
+        }
+
+        public int Round
+        {
+            get { return round; }
+        }
+
+        ///
+
+        public string DisplayMap()
+        {
+            return map.DisplayMap(); //map. allows form to access it
+        }
+
+        public string Information()
+        {
+            string info = ""; //making the space between each unit
+            foreach (Unit unit in map.Units)
+            {
+                info += unit + "\n";
+            }
+            return info;
+        }
+        ///
+
+        public void Reset() //reset the map for a new simulation
+        {
+            map.Reset();
+            gameOver = false;
+            round = 0;
+        }
+
+        ///
+
+        public void GameLoop()
+        {
+            foreach (Unit unit in map.Units) //link all units in map.Units
+            {
+                if (unit.IsDestroyed)
+                {
+                    continue; //guarding if statement: if it is destroyed, go to the next unit in the loop
+                }
+
+                Unit closest = unit.GetClosestUnit(map.Units);
+                if (closest == null) //avoiding nesting more than 2 if statements (no spaghetti!)
+                {
+                    gameOver = true;
+                    winning = unit.Faction;
+                    map.UpdateDisplay();
+                    return;
+                }
+
+                double percentageHealth = unit.Health / unit.MaxHealth;
+                if (percentageHealth <= 0.25) //if health percentage is less than 25%, it must run randomly away
+                {
+                    unit.Run();
+                }
+                else if (unit.InRange(closest))
+                {
+                    unit.Combat(closest);
+                }
+                else
+                {
+                    unit.Move(closest);
+                }
+                MapBoundary(unit, map.Size); //if x < 0, reset x to 0 so it never leaves the map (push unit back in)
+            }
+        
+            map.UpdateDisplay();
+            round++;
+        }
+
+        ///
+
+        private void MapBoundary(Unit unit, int size)
+        {
+            if (unit.X < 0) //push in x
+            {
+                unit.X = 0;
+            }
+            else if (unit.X >= size)
+            {
+                unit.X = size - 1;
+            }
+            if (unit.Y < 0) //push in y
+            {
+                unit.Y = 0;
+            }
+            else if (unit.Y >= size)
+            {
+                unit.Y = size - 1;
+            }
+        }
     }
 }
