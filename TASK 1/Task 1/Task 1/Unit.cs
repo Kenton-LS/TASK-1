2198c203dfa79f8aad8e77e32af89263877c9cf0
Kenton <kentonlesliesmith@gmail.com>
2019-10-03 20:25:19 +0200

    Updated the Unit Class
    Improved overall code. Polished the initial variable declarations, added public abstract [get,set] for every int, char, and string. Created a GetDistace method, and constructed a ToString().

--- TASK 1/Task 1/Task 1/Unit.cs
+++ TASK 1/Task 1/Task 1/Unit.cs
@@ -6,44 +6,60 @@ using System.Threading.Tasks;
 
 namespace Task_1
 {
-    abstract class Unit : Map
+    abstract class Unit
     {
-        protected int x, y, health, maxHealth, speed, attack, attackRange; //protected
-        protected string faction, symbol;//protected
-        protected bool isAttacking;
+        protected int x, y, health, maxHealth, speed, attack, attackRange;
+        protected string faction;
+        protected char symbol; //Mellee or ranged
+        protected bool isAttacking = false; //set to false by default, doesn't need a parameter
+        protected bool isDestroyed = false;
+        public static Random r = new Random(); //to enable random in all classes
 
-        //ThisWillProbablyMakeMisterHenkVeryVeryMad()
-        
-        public int X { set { x = value; } }
-        public int Y { set { y = value; } }
-        public string Faction { set { faction = value; } }
-        
-
-        public abstract void Move();
-        
-        public abstract void Combat();
-
-        public abstract void InAttackRange();
-
-        public abstract void ReturnPosition();
-
-        public abstract void Death();
-
-        public abstract string ToString();
-
-        public Unit(int x, int y, int health, int maxHealth, int speed, int attack, int attackRange, string faction, string symbol)
+        public Unit(int x, int y, int health, int maxHealth, int speed, int attack, int attackRange, char symbol, string faction) //CONSTRUCTOR
         {
-            this.x = x;
+            this.x = x; //Initialize everything
             this.y = y;
             this.health = health;
-            this.maxHealth = maxHealth;
+            this.maxHealth = health; //NB set to health initially, giving a total
             this.speed = speed;
             this.attack = attack;
             this.attackRange = attackRange;
-            this.faction = "?";
-            this.symbol = "?";
+            this.faction = faction;
+            this.symbol = symbol;
         }
 
+        public abstract int X { get; set; }
+        public abstract int Y { get; set; }
+        public abstract int Health { get; set; }
+        public abstract int MaxHealth { get; }
+        //public abstract int Speed { get; set; } //these are experimental, but not necessary
+        //public abstract int Attack { get; set; }
+        //public abstract int AttackRange { get; set; }
+        public abstract char Symbol { get; }
+        public abstract string Faction { get; }
+        public abstract bool IsDestroyed { get; }
+
+
+        public abstract void Move(Unit closestUnit); //Abstract method declarations
+        public abstract void Combat(Unit otherUnit);
+        public abstract void Run();
+        public abstract bool InRange(Unit otherUnit); //Returns a boolean
+        public abstract Unit GetClosestUnit(Unit[] units); //Returns Units, takes an array of Units
+        public abstract void Kill();
+
+
+        protected double GetDistance(Unit otherUnit) //helper method
+        {
+            double xDistance = otherUnit.X - X; //get x distance
+            double yDistance = otherUnit.Y - Y; //get y distance
+            return Math.Sqrt(xDistance * xDistance + yDistance * yDistance); //more efficient than Math.Pow
+        }
+
+        public override string ToString() //Rather place ToString here instead of dupicating it in other classes (Ranged & Melee)
+        {
+            return "X: " + x + " Y: " + y + "\n" +
+                   "HP:  " + health + " / " + maxHealth + "\n" +
+                   "FACTION:  " + faction + "\nSYMBOL:  " + symbol + "\n";
+        }
     }
 }
-
